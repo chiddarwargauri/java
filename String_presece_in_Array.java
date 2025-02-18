23196b003d953c64e79410ea3d2e129317fb379a
chiddarwargauri <chiddarwargauri@gmail.com>
2025-02-18 23:18:57 +0530

    serch string

--- String_presece_in_Array.java
+++ String_presece_in_Array.java
@@ -3,6 +3,19 @@ public class String_presece_in_Array {
 
         String[] s={"gauri","prakash","chiddarwar"};
         String check="gauri";
+        boolean flag= false;
+
+        for (String i:s){
+            if(i.equals(check)){
+                System.out.println("String found in array");
+                flag=true;
+                break;
+            }
+        }
+        if(flag==false)
+        {
+            System.out.println("String not found");
+        }
         
         
     }
